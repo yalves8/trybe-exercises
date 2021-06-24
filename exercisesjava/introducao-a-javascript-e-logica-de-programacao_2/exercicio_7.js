494a8d0cf2a51cc3aba34ae73d6b3c04b54626d6
Yasmin <yasminleticia_@hotmail.com>
2021-06-24 16:39:32 -0300

    modificações no 5 e 7 menor

--- exercisesjava/introducao-a-javascript-e-logica-de-programacao_2/exercicio_7.js
+++ exercisesjava/introducao-a-javascript-e-logica-de-programacao_2/exercicio_7.js
@@ -1,9 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 200];
+
 let caixa = numbers[0];
 
 for(let i =0;i<numbers.length;i+=1){
     
-    if(caixa < numbers[i]){
+    if(caixa > numbers[i]){
         caixa = numbers[i];
     }
     
