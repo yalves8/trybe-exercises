890664b074501314d8604ea94befc2cf0e5d68a9
Yasmin <yasminleticia_@hotmail.com>
2021-06-24 16:33:15 -0300

    pegar o maior em duas linhas

--- exercisesjava/introducao-a-javascript-e-logica-de-programacao_2/exercicio_5.js
+++ exercisesjava/introducao-a-javascript-e-logica-de-programacao_2/exercicio_5.js
@@ -1,5 +1,5 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 200];
-let caixa = 5;
+/*let caixa = 5;
 
 for(let i =0;i<numbers.length;i+=1){
     
@@ -9,4 +9,7 @@ for(let i =0;i<numbers.length;i+=1){
     
 }
 
-console.log(caixa);
+console.log(caixa);*/
+
+numero2 = numbers.sort();
+console.log(numero2[0]);
