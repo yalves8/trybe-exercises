8f5eb4764a0a330aaccd730331d24c4e4762b1f4
Yasmin <yasminleticia_@hotmail.com>
2021-06-24 17:19:43 -0300

    menor pro maior

--- exercisesjava/introducao-a-javascript-e-logica-de-programacao_2/exercicio_10.js
+++ exercisesjava/introducao-a-javascript-e-logica-de-programacao_2/exercicio_10.js
@@ -11,8 +11,8 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 200];
     }
   }*/
 
-  for (let index = 0; index<numbers.length; index +=1){
-      for (let index2 = 1; index2<index;index+=1){
+  for (let index = 1; index<numbers.length; index +=1){
+      for (let index2 = 0; index2<index;index2 +=1){
           if(numbers[index] < numbers[index2]){
               let position = numbers[index];
               numbers[index] = numbers[index2];
@@ -24,5 +24,5 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 200];
       
   }
 
-  console.log("oi");
+  console.log(numbers);
   
