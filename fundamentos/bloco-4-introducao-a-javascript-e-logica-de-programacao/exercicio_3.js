370d47c281d622a1d9ab8ae1a88587e32533a8a8
Yasmin <yasminleticia_@hotmail.com>
2021-06-25 17:41:08 -0300

    identações e comentários

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/exercicio_3.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/exercicio_3.js
@@ -10,10 +10,10 @@ for (let i = 1; i <= n; i += 1) {
         console.log(espaco);
     }
 
-    for (let j = 1; j <= n - i; j += 1) { //vai de 1 até n - i para colocar os espaços
+    for (let colocaEspaco = 1; colocaEspaco <= n - i; colocaEspaco += 1) { //vai de 1 até n - i para colocar os espaços
         espaco += ' '; //seta o espaço sempre pra não acumular
-        if (j == n - i) {  // quando chega no limite, usa o restante do tamanho para colocar os asteristicos
-            for (let y = j; y < n; y += 1) {
+        if (colocaEspaco == n - i) {  // quando chega no limite, usa o restante do tamanho para colocar os asteristicos
+            for (let colocaAsteristico = colocaEspaco; colocaAsteristico < n; colocaAsteristico += 1) {
                 espaco += '*';
             }
             console.log(espaco);
