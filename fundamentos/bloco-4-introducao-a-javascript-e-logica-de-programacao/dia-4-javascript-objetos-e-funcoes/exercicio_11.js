53b5126b0d39dcba0c994dec75ec7e0dae0f5621
Yasmin <yasminleticia_@hotmail.com>
2021-06-29 15:16:12 -0300

    inicio basket

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio_11.js
@@ -0,0 +1,45 @@
+const basket = [
+    'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+    'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+    /*'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+    'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+    'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+    'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+    'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+    'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+    'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+    'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+    'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+    'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+    'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+    'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+    'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+    'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+    'Banana', 'Pera', 'Abacate', 'Uva',*/
+];
+
+
+let array = ['Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva'];
+
+let cont = 0;
+let nomes = [];
+for (let i in basket) {
+    //j = 0;
+    for (let j in basket) {
+        if (basket[i] == basket[j]) {
+            if(i == 0){
+                nomes.push(basket[i]);
+                basket[j] = '*';
+            }
+            else if(nomes.indexOf(basket[i]) != basket[j]){
+                nomes.push(basket[i]);
+                basket[j] = '*';
+                
+            }
+            
+
+        }
+    }
+}
+
+console.log(nomes);
