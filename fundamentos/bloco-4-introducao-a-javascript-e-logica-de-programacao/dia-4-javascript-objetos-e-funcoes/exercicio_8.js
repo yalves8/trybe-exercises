af314d17e41f3fb62a5b933643450e42ffc41580
Yasmin <yasminleticia_@hotmail.com>
2021-06-29 07:14:39 -0300

    .

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio_8.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio_8.js
@@ -9,27 +9,19 @@ function numeroRepetido(array) {
                 if (i != rep) { // verifica se os indices sao iguais
                     cont += 1;
                 }
-
             }
-
         }
         vetor.push(cont);
-
     }
 
     let max = Math.max.apply(null, vetor); //pega o numero maior das repeticoes
 
-    //console.log(max);
-    
     for (let indice in vetor) {  //procura no vetor feito os indices
-
         if (vetor[indice] == max) { //se o valor for o numero max de repeticao
             return array[indice]; // pega o indice do valor e puxa o mesmo indice do array
-            
+
         }
     }
-
-
 }
 
 console.log(numeroRepetido([2, 3, 2, 5, 8, 2, 3]));
