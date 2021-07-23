a076fff095d700eae829680f9e414300e74870b1
Yasmin <yasminleticia_@hotmail.com>
2021-07-22 23:17:22 -0300

    ordenar

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -11,8 +11,13 @@
 //     console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
 //   }
 
-  
-  const testingScope = (escopo) => ((escopo === true) ? 'Não devo ser utilizada dora do meu escopo(if) ótimo, fui utilizada no escopo !': 'Não devo ser utilizada fora meu escopo (else)');
-  console.log(testingScope(true));
 
-  
+const testingScope = (escopo) => ((escopo === true) ? 'Não devo ser utilizada dora do meu escopo(if) ótimo, fui utilizada no escopo !' : 'Não devo ser utilizada fora meu escopo (else)');
+console.log(testingScope(true));
+
+
+const num = [13, 3, 4, 10, 7, 2];
+
+const oddsAndEvens = (array) => array.sort((a,b) => a-b);
+
+console.log(oddsAndEvens(num))
