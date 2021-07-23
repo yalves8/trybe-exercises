a896564c6a98f0a8d1b708db6d7ecf5e413fd0b1
Yasmin <yasminleticia_@hotmail.com>
2021-07-22 23:06:51 -0300

    primeira parte

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -0,0 +1,16 @@
+// function testingScope(escopo) {
+
+//     if (escopo === true) {
+//       var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//       ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+//       console.log(ifScope);
+//     } else {
+//       var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//       console.log(elseScope);
+//     }
+//     console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+//   }
+
+  
+  const testingScope = (escopo) => ((escopo === true) ? 'Não devo ser utilizada dora do meu escopo(if) ótimo, fui utilizada no escopo !': 'Não devo ser utilizada fora meu escopo (else)');
+  console.log(testingScope(true));
