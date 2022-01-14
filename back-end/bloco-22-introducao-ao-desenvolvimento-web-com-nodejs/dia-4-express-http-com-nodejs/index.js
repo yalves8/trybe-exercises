7cae9ce4ad1a38b2b1e40198a410897581d5a25d
yasmin <yasminleticia_@hotmail.com>
2022-01-13 23:01:45 -0300

    exercicio 2 post

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -3,10 +3,17 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/ping', function(req, res) {
+//exercicio1
+app.get('/ping', function(_req, res) {
     return res.status(200).json({message: 'pong'});
 });
 
-app.listen(3001), () => {
+//exercicio2
+app.post('/hello', function(req,res) {
+    const {name} = req.body;
+    return res.status(200).json({message : `Hello, ${name}!`});
+})
+
+app.listen(3001, () => {
     console.log('Aplicação na porta 3001 ');
-}
+});
