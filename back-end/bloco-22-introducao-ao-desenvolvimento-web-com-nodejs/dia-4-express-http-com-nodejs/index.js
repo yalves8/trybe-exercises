d4da0bd4857ee49ba71d20306f4192b3f965eb9b
yasmin <yasminleticia_@hotmail.com>
2022-01-13 23:07:10 -0300

    exercicio 3 idade autorizada

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -12,7 +12,14 @@ app.get('/ping', function(_req, res) {
 app.post('/hello', function(req,res) {
     const {name} = req.body;
     return res.status(200).json({message : `Hello, ${name}!`});
-})
+});
+
+//exercicio3
+app.post('/greetings', function(req,res) {
+    const {name, age} = req.body;
+    if(age > 17)return res.status(200).json({message : `Hello, ${name}!`});
+    res.status(401).json({message: 'Unauthorized '});
+});
 
 app.listen(3001, () => {
     console.log('Aplicação na porta 3001 ');
