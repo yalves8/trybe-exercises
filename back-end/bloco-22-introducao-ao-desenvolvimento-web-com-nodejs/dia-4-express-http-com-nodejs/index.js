26e22d3bd3272060e5d6c6ccb8ec44026d2570fe
yasmin <yasminleticia_@hotmail.com>
2022-01-14 00:24:17 -0300

    exercicio 5 cria api e testa

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -2,23 +2,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
+const { updateJson, readJson } = require('./indexSimpson');
 
 //exercicio1
-app.get('/ping', function(_req, res) {
-    return res.status(200).json({message: 'pong'});
+app.get('/ping', function (_req, res) {
+    return res.status(200).json({ message: 'pong' });
 });
 
 //exercicio2
-app.post('/hello', function(req,res) {
-    const {name} = req.body;
-    return res.status(200).json({message : `Hello, ${name}!`});
+app.post('/hello', function (req, res) {
+    const { name } = req.body;
+    return res.status(200).json({ message: `Hello, ${name}!` });
 });
 
 //exercicio3
-app.post('/greetings', function(req,res) {
-    const {name, age} = req.body;
-    if(age > 17)return res.status(200).json({message : `Hello, ${name}!`});
-    res.status(401).json({message: 'Unauthorized '});
+app.post('/greetings', function (req, res) {
+    const { name, age } = req.body;
+    if (age > 17) return res.status(200).json({ message: `Hello, ${name}!` });
+    res.status(401).json({ message: 'Unauthorized ' });
+});
+
+
+//exercicio5
+app.get('/simpsons', function (req, res) {
+    readJson().then(() => res.status(200).json({message: 'Deu certo!' })).catch((err) => res.status(500).json({message: `${err.message}`}));    
 });
 
 app.listen(3001, () => {
